201c0b500daac9973be24d9f53f4b94d9d035a00
NekroMagus <nekromagus@mail.ru>
2019-07-23 15:39:00 +0300

    add annotations and fields

--- backend/src/main/java/net/store/domain/Customer.java
+++ backend/src/main/java/net/store/domain/Customer.java
@@ -1,13 +1,29 @@
 package net.store.domain;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
-@Table(name = "Customer")
+@Table(name = "customers")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private long id;
 
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
 
+    @Column(name = "second_name", nullable = false)
+    private String secondtName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    private String password;
+
+    private List<Order> orderList;
 }
