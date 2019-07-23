dd100a790c42e2df8a35b1116175bea56832a8e3
NekroMagus <nekromagus@mail.ru>
2019-07-23 15:18:32 +0300

    Update Customer

--- backend/src/main/java/net/store/domain/Customer.java
+++ backend/src/main/java/net/store/domain/Customer.java
@@ -2,6 +2,12 @@ package net.store.domain;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "Customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
 
 }
