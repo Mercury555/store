4210b87b470f3118ab4cf26761dc225efed67134
total <8432@mail.ru>
2019-07-24 16:46:29 +0300

    Add customers class. Test new fork.

--- backend/src/main/java/net/store/domain/Customer.java
+++ backend/src/main/java/net/store/domain/Customer.java
@@ -1,5 +1,86 @@
 package net.store.domain;
 
-public class Customer {
+import javax.persistence.*;
+import java.util.List;
 
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "second_name", nullable = false)
+    private String secondtName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    private List<Order> orderList;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondtName() {
+        return secondtName;
+    }
+
+    public void setSecondtName(String secondtName) {
+        this.secondtName = secondtName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Order> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<Order> orderList) {
+        this.orderList = orderList;
+    }
 }
