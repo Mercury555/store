58529a5235ddb2a2681ce337266f5da658440625
NekroMagus <nekromagus@mail.ru>
2019-08-06 20:57:02 +0300

    Add Role Admin and controller

--- backend/src/main/java/net/store/service/CustomerService.java
+++ backend/src/main/java/net/store/service/CustomerService.java
@@ -2,7 +2,15 @@ package net.store.service;
 
 import net.store.domain.Customer;
 
+import java.util.List;
+
 public interface CustomerService {
+
+    void saveCustomer(Customer customer);
+
+    void deleteCustomerById(long id);
+
+    List<Customer> findAll();
 
     Customer findCustomerByUsername(String username);
 }
--- backend/src/main/java/net/store/service/CustomerServiceImpl.java
+++ backend/src/main/java/net/store/service/CustomerServiceImpl.java
@@ -5,11 +5,28 @@ import net.store.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomerServiceImpl implements CustomerService{
 
     @Autowired
     private CustomerRepository repository;
+
+    @Override
+    public void saveCustomer(Customer customer) {
+        repository.save(customer);
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public void deleteCustomerById(long id) {
+        repository.delete(repository.findById(id).get());
+    }
 
     @Override
     public Customer findCustomerByUsername(String username) {
